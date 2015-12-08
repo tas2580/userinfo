76c5bfccb4cb0157a520cbf8f6b1129e02cf489d
tas2580 <tobi.schaefer@gmail.com>
2015-12-08 22:35:23 +0100

    optimized JS

--- styles/all/template/script.js
+++ styles/all/template/script.js
@@ -1,7 +1,6 @@
 (function($) {
 	'use strict';
 
-
 	/* Set div to mouse */
 	$(document).mousemove( function(e) {
 		$('#popup').css({'top':e.pageY+20,'left':e.pageX+10});
@@ -10,10 +9,26 @@
 	$("a.username-coloured, a.username").mouseover(display_userinfo);
 	$("a.username-coloured, a.username").mouseout(hide_userinfo);
 
+	/* Display the info popup */
+	function display_userinfo(){
+		show_popup = true;
+		var id = getURLParameter(($(this).attr('href')), 'u');
+		var url = userinfo_url.replace('USERID', id);
 
+		$.get(url, function( responseText ) {
+			var data = eval (responseText);
+			$.each(data[0], function(index, value){
+				$('#ajax_'+index).html(value);
+			});
+
+			if (show_popup) {
+				$( "#popup" ).show();
+			}
+		});
+	}
 
 	/* Hide the info popup */
-	function hide_userinfo(){
+	function hide_userinfo() {
 		show_popup = false;
 		$( "#popup" ).hide();
 		$( "#popup" ).find('span').each(function(){
