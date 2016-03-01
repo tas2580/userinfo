7112b2a3626aa4467958b5f7d1d4e0f3f700cf3f
tas2580 <tobi.schaefer@gmail.com>
2016-03-01 23:34:41 +0100

    popup switch to right if no space on left side and user caching

--- styles/all/template/script.js
+++ styles/all/template/script.js
@@ -3,21 +3,44 @@
 
 	/* Set div to mouse */
 	$(document).mousemove( function(e) {
-		$('#popup').css({'top':e.pageY+20,'left':e.pageX+10});
+		var docX, docY, pos;
+		if(e) {
+			if(typeof(e.pageX) === 'number') { docX = e.pageX; docY = e.pageY;}
+			else {docX = e.clientX; docY = e.clientY;}
+		} else {
+			docY += $(window).scrollTop();
+			docX += $(window).scrollLeft();
+		}
+		if (docX > $(window).width() - 400) {
+			pos = (docX - 350);
+		} else {
+			pos = (docX - 15);
+		}
+
+		$('#popup').css({'top':(docY+35),'left':pos});
 	});
 	var timeout;
+	var data = new Array();
 	$("a.username-coloured, a.username").hover(
 		function() {
 			var id = getURLParameter(($(this).attr('href')), 'u');
 			var url = userinfo_url.replace('USERID', id);
+
 			timeout = setTimeout(function(){
-				$.get(url, function( responseText ) {
-					var data = responseText;
-					$.each(data, function(index, value){
+				if(typeof data[id] === 'undefined') {
+					$.get(url, function( responseText ) {
+						data[id] = responseText;
+						$.each(data[id], function(index, value){
+							$('#ajax_'+index).html(value);
+						});
+						$( "#popup" ).show();
+					});
+				} else {
+					$.each(data[id], function(index, value){
 						$('#ajax_'+index).html(value);
 					});
 					$( "#popup" ).show();
-				});
+				}
 			}, 500);
 		},
 		function(){
